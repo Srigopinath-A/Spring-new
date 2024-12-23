665e0b2be9738fa10d19fb6567784a1a26744ef1
Srigopinath A <111897270+Srigopinath-A@users.noreply.github.com>
2024-12-23 07:31:13 +0000

    Add files via upload

---
+++ SpringApps-main/SpringApps-main/codegen/codegen/src/test/java/com/codegen/app/backend/codegen/CodegenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegen.app.backend.codegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodegenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
